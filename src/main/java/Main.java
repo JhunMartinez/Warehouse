5d93ae78510ce9e4f107e6a60d68e538f08583b2
jhunmartinez <jhunmartinez1@gmail.com>
2021-05-29 17:19:02 -0400

    Warehouse commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,19 +3,15 @@
 public class Main {
 
     public static void main(String[] args) {
-        // here you can write code to test your classes
-// the usual:
+
+        //Gets a product from the warehouse along with capacity and balance amount
         ProductWarehouseWithHistory juice = new ProductWarehouseWithHistory("Juice", 1000.0, 1000.0);
         juice.takeFromWarehouse(11.3);
         System.out.println(juice.getName()); // Juice
         juice.addToWarehouse(1.0);
-        System.out.println(juice);           // Juice: balance = 989.7, space left 10.3
+        System.out.println(juice);
 
-// etc
-
-// however, history() still doesn't work properly:
-        System.out.println(juice.history()); // [1000.0]
-// so we only get the initial state of the history set by the constructor...
+        System.out.println(juice.history());
     }
 
 }
